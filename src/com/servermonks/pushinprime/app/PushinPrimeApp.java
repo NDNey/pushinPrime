2c8809ac482b290b2f039cffe5b38805d06b2035
kriskraw <chriscrawley80@gmail.com>
2022-06-07 18:31:09 -0400

    Group coding. Restructured project. Added Board, Colors, Game, Main, & PushinPrimeApp classes

--- src/com/servermonks/pushinprime/app/PushinPrimeApp.java
+++ src/com/servermonks/pushinprime/app/PushinPrimeApp.java
@@ -1,8 +1,47 @@
 package com.servermonks.pushinprime.app;
 
-import com.servermonks.pushinprime.Game;
-
 public class PushinPrimeApp {
 
+    private Prompter prompter;
+    private final Board board = Board.getInstance();
+    private boolean gameOver;
 
+
+    public PushinPrimeApp(Prompter prompter) {
+        this.prompter = prompter;
+    }
+
+    /*
+     * Initial game execution:
+     *  -> displays welcome banner, instructions and promps for player names
+     */
+    public void execute() throws IOException {
+        welcome();
+        //howToPlay();
+        prompter.prompt(GREEN + "Press [enter] to start..." + RESET + "");
+        //showSplashScreen();
+        //createBoard();
+        //prompts for user name
+        //prompt for commands
+        //promptForUsername();
+        //run();
+    }
+
+    private void welcome() throws IOException {
+        Console.clear();
+        String banner = Files.readString(Path.of("resources/welcome_banner.txt"));
+        prompter.info(banner);
+    }
+
+    public void getCommands() {
+        while (gameOver != true) {
+            Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+            System.out.println("Enter username");
+            String userName = myObj.nextLine();
+            //need to use .split("",2) to split input into two.
+            //take seperate actions depending on input(grab,use,go)
+
+
+        }
+    }
 }
--- src/com/servermonks/pushinprime/client/Main.java
+++ src/com/servermonks/pushinprime/client/Main.java
@@ -1,8 +1,11 @@
 package com.servermonks.pushinprime.client;
 
+import com.servermonks.pushinprime.app.PushinPrimeApp;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        PushinPrimeApp pushinPrimeApp = new PushinPrimeApp();
+        pushinPrimeApp.execute();
     }
 }
