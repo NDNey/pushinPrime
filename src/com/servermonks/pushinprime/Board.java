8ed26d7d775baa0a6d0f1c54cdf828d32f94ad32
Mannie004 <seun.adeyemi04@yahoo.com>
2022-06-09 10:58:21 -0400

    Included a gameover/replay criteria AB#6

--- src/com/servermonks/pushinprime/Board.java
+++ src/com/servermonks/pushinprime/Board.java
@@ -11,29 +11,32 @@ public class Board {
     JTextField commandInput = new JTextField();
     JTextArea commandInfo = new JTextArea();
 
-    public void Board() {
-        createBoard();
+//    public void Board () {
+//        createBoard();
+//    }
+    public static Board getInstance() {
+        return new Board();
     }
 
-    public void createBoard() {
-
-        frame = new JFrame("pushinPRIME :)");
-        frame.setSize(500, 500);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        sysOut = new JTextArea(50, 10);
-        frame.getContentPane().add(sysOut);
-
-        commandInfo = new JTextArea();
-
-        commandInput = new JTextField(50);
-        Color c = new Color(0,0,0,100);
-        commandInput.setBackground(c);
-
-        frame.pack();
-        frame.setVisible(true);
-
-        PrintStream printStream = new PrintStream(new JoutputStream(sysOut));
-        System.setOut(printStream);
-    }
+//    public void createBoard() {
+//
+//        frame = new JFrame("pushinPRIME :)");
+//        frame.setSize(500, 500);
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//
+//        sysOut = new JTextArea(50, 10);
+//        frame.getContentPane().add(sysOut);
+//
+//        commandInfo = new JTextArea();
+//
+//        commandInput = new JTextField(50);
+//        Color c = new Color(0,0,0,100);
+//        commandInput.setBackground(c);
+//
+//        frame.pack();
+//        frame.setVisible(true);
+//
+//        PrintStream printStream = new PrintStream(new JoutputStream(sysOut));
+//        System.setOut(printStream);
+//    }
 }
