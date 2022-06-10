2b1ddf1b182330d0972995f126e380e9e4377103
NDNey <ndavidney@gmail.com>
2022-06-10 13:34:28 -0400

    help function added  AB#7

--- src/com/servermonks/pushinprime/client/Main.java
+++ src/com/servermonks/pushinprime/client/Main.java
@@ -8,8 +8,7 @@ import java.io.IOException;
 public class Main {
 
 
-
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
 
         PushinPrimeApp pushinPrimeApp = new PushinPrimeApp();
         try {
