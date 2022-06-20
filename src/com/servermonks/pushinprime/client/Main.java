40adf4b9557b5034b24e161136b1133641c906e0
NDNey <ndavidney@gmail.com>
2022-06-20 16:11:11 -0400

    color bug fixed in play again

--- src/com/servermonks/pushinprime/client/Main.java
+++ src/com/servermonks/pushinprime/client/Main.java
@@ -7,11 +7,8 @@ public class Main {
     public static void main(String[] args) {
 
         PushinPrimeApp pushinPrimeApp = new PushinPrimeApp();
-
         try {
-
             pushinPrimeApp.execute();
-
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
